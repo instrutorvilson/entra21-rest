55ce9ef40ef6c2cb9f0a952d74eed9493e26ab4f
vilsonmoro <vilsonmoro@gmail.com>
2023-12-06 14:22:20 -0300

    add controller com parâmetros (requestParam)

--- src/main/java/com/example/aula/controllers/HomeController.java
+++ src/main/java/com/example/aula/controllers/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,8 +14,16 @@ public class HomeController {
 	/*http://localhost:8080*/
 	@GetMapping("/ola")
 	public String olaMundo() {
-		return "Hello world";
+		return "Hello world ";
 	}
+	
+	@GetMapping("/ver")
+	public String olaMundoc(@RequestParam("nome") String nome, @RequestParam("idade") int idade ) {
+		String msg = idade < 17 ? "menor" : "maior";		
+		//return nome + " com " + idade + " vc é " + msg;
+		return msg = String.format("%s com %s anos vc é %s de idade",nome, idade, msg);
+	}
+	
 	
 	@GetMapping("/ola/{nome}")
 	public String olaMundo(@PathVariable("nome") String nome) {
