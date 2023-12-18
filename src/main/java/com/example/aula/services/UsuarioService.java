96e298565e4db2b623faf37f6ee6c7ad4bf61e64
vilsonmoro <vilsonmoro@gmail.com>
2023-12-18 11:10:59 -0300

    Adicionar criptografia de senha

--- src/main/java/com/example/aula/services/UsuarioService.java
+++ src/main/java/com/example/aula/services/UsuarioService.java
@@ -3,6 +3,10 @@ package com.example.aula.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.aula.entidades.Usuario;
@@ -10,19 +14,29 @@ import com.example.aula.exceptions.RecursoJaExistente;
 import com.example.aula.repositories.UsuarioRepository;
 
 @Service
-public class UsuarioService {
+public class UsuarioService implements UserDetailsService{
 	@Autowired
 	UsuarioRepository repo;
+	
+	@Autowired
+	BCryptPasswordEncoder passwordEnconder;
 
 	public Usuario salvar(Usuario usuario) {
 		if(repo.findByEmail(usuario.getEmail()) != null) {
 		  throw new RecursoJaExistente("Já existe um usuário com o email informado");	
 		}
+		usuario.setSenha(passwordEnconder.encode(usuario.getSenha()));
 		repo.save(usuario);
 		return usuario;
 	}
 	
 	public List<Usuario> consultar(){
 		return repo.findAll();
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Usuario user = repo.findByEmail(username);
+		return user;
 	}
 }
