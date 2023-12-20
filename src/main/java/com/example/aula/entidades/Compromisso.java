46f95249cfbed3b1582a0582c750cd52d24d7a41
vilsonmoro <vilsonmoro@gmail.com>
2023-12-20 11:41:32 -0300

    ajustes de data e hora

--- src/main/java/com/example/aula/entidades/Compromisso.java
+++ src/main/java/com/example/aula/entidades/Compromisso.java
@@ -1,17 +1,19 @@
 package com.example.aula.entidades;
 
-import java.sql.Date;
 import java.sql.Time;
+import java.util.Date;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.Temporal;
-import jakarta.persistence.TemporalType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Table(name = "tb_compromissos")
@@ -23,7 +25,8 @@ public class Compromisso {
 	private String descricao;
 	@Temporal(TemporalType.DATE)
 	private Date data;
-	@Temporal(TemporalType.TIME)
+	
+	@JsonFormat(pattern = "HH:mm:ss")
 	private Time hora;
 	
 	@ManyToOne
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,8 +2,8 @@ insert into tb_contatos(id,nome,email,fone)values(1,'maria','maria@gmail.com','(
 insert into tb_contatos(id,nome,email,fone)values(2,'jose','jose@gmail.com','(47)9809-0987');
 insert into tb_contatos(id,nome,email,fone)values(3,'joao','joao@gmail.com','(47)9809-0987');
 
-insert into tb_compromissos(id,descricao,data,hora,contato_id)values(10, 'Jogar bocha','2023-12-11','15:00:00',1);
-insert into tb_compromissos(id,descricao,data,hora,contato_id)values(20, 'pescar','2023-10-11','15:00:00',2);
+insert into tb_compromissos(id,descricao,data,hora,contato_id)values(10, 'Jogar bocha','2023-12-11', '15:00:00',1);
+insert into tb_compromissos(id,descricao,data,hora,contato_id)values(20, 'pescar','2023-10-11','18:00:00',2);
 
 insert into tb_user(nome, email, senha)values('maria','maria@gmail.com','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
 insert into tb_user(nome, email, senha)values('joana','joana@gmail.com','$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
