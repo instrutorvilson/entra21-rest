83af925d0c05e701ab6410478353928a0297a75e
vilsonmoro <vilsonmoro@gmail.com>
2023-12-20 10:55:31 -0300

    Criado autorization resource

---
+++ src/main/java/com/example/aula/config/ResourceServerConfig.java
@@ -0,0 +1,76 @@
+package com.example.aula.config;
+
+import java.util.Arrays;import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+@Configuration
+@EnableResourceServer
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+	/*classe que recebe o token e verifica as permissões do usuario*/
+    @Autowired
+    private Environment env;
+  
+    @Autowired
+    private JwtTokenStore tokenStore;
+
+    //arrays com as permissões
+    private static final String[] PUBLIC = {"/oauth/token","/h2-console/**","/api/**"};
+    private static final String[] ADMIN = {"/contatos", "/usuarios"};
+    private static final String[] USUARIO = {"/compromissos"};
+
+    @Override //recebe o token da requisição
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.tokenStore(tokenStore);
+    }
+
+    @Override //configura as rotas
+    public void configure(HttpSecurity http) throws Exception {
+        if(Arrays.asList(env.getActiveProfiles()).contains("test")){
+            http.headers().frameOptions().disable();
+        }
+         http.authorizeRequests()
+                .antMatchers(PUBLIC).permitAll() //define as autorizações
+                .antMatchers(HttpMethod.GET, USUARIO).hasRole("USUARIO")
+                .antMatchers(HttpMethod.POST, USUARIO).hasRole("ADMIN")
+                .antMatchers(ADMIN).hasRole("ADMIN")
+                .anyRequest().authenticated();
+
+            http.cors().configurationSource(corsConfigurationSource());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOriginPatterns(Arrays.asList("*"));
+        corsConfig.setAllowedMethods(Arrays.asList("POST", "GET", "PUT", "DELETE", "PATCH"));
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return source;
+    }
+    
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> bean
+                = new FilterRegistrationBean<>(new CorsFilter(corsConfigurationSource()));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return bean;
+    }
+}
