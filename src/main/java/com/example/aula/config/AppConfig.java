4d1c4d68abdde4cfcfa2504a750319674b925cf9
vilsonmoro <vilsonmoro@gmail.com>
2023-12-20 10:31:19 -0300

    Ajustes de beans

--- src/main/java/com/example/aula/config/AppConfig.java
+++ src/main/java/com/example/aula/config/AppConfig.java
@@ -1,13 +1,35 @@
 package com.example.aula.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
 
 @Configuration
 public class AppConfig {
+  /*
+     //ler variaveis de ambiente
+     @Value("${jwt.secret}")   
+      private String jwtSecret;
+   */
+	
    @Bean
    public BCryptPasswordEncoder passwordEncoder() {
 	   return new BCryptPasswordEncoder();
+   }
+   
+   @Bean
+   public JwtAccessTokenConverter accessTokenConverter() {
+	   JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
+	   tokenConverter.setSigningKey("entra21");
+	   return tokenConverter;
+   }
+   
+   @Bean
+   public JwtTokenStore tokenStore(){
+       return new JwtTokenStore(accessTokenConverter());
    }
 }
