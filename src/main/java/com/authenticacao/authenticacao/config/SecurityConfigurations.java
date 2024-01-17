0672ae23f25c605ec201abbe66edb16191b7ade2
vilsonmoro <vilsonmoro@gmail.com>
2024-01-17 09:35:48 -0300

    gerar token

--- src/main/java/com/authenticacao/authenticacao/config/SecurityConfigurations.java
+++ src/main/java/com/authenticacao/authenticacao/config/SecurityConfigurations.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -30,5 +32,10 @@ public class SecurityConfigurations {
    @Bean
    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
 	   return authenticationConfiguration.getAuthenticationManager();
+   }
+   
+   @Bean
+   public PasswordEncoder passwordEncoder(){
+       return new BCryptPasswordEncoder();
    }
 }
--- src/main/java/com/authenticacao/authenticacao/config/TokenService.java
+++ src/main/java/com/authenticacao/authenticacao/config/TokenService.java
@@ -47,6 +47,6 @@ public class TokenService {
 	}
 
 	private Instant genExpirationDate() {
-		return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-3:00"));
+		return LocalDateTime.now().plusHours(1).toInstant(ZoneOffset.of("-03:00"));
 	}
 }
