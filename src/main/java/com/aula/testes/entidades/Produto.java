009074b4ab925b14bb1a2548b2c7f1a7c84b7adc
vilsonmoro <vilsonmoro@gmail.com>
2024-02-07 11:22:41 -0300

    configurar perfil e variaveis de ambiente

---
+++ src/main/java/com/aula/testes/entidades/Produto.java
@@ -0,0 +1,44 @@
+package com.aula.testes.entidades;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_produtos")
+public class Produto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	@Column(length = 50)
+	private String nome;
+	private double preco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+}
